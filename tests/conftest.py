b66db762dee975d9eb1e905a310db97ddf09e25b
vadim_makarov <makarov.vadim.evph@gmail.com>
2025-03-30 14:27:07 +0300

    Добавил test_on_push.yaml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import pytest
 
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session")
 def browser():
    print("\nstart browser for test..")
-   browser = webdriver.Chrome()
+   options = Options()
+   options.add_argument('--headless')
+   browser = webdriver.Chrome(options=options)
    yield browser
    print("\nquit browser..")
    browser.quit()
