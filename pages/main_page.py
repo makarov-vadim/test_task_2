a8606c9649832802354bd7b7ccf7f8fddbe1ca1d
vadim_makarov <makarov.vadim.evph@gmail.com>
2025-03-31 19:40:32 +0300

    Исправления по ревью

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,8 @@
-from config.config import URLS
-from helpers.base_page import BasePage
 from selenium.webdriver.common.by import By
+
+from config.config import URLS
+from helpers.help_functions import get_first_name, get_post_code
+from pages.base_page import BasePage
 
 
 class Locators:
@@ -27,6 +29,58 @@ class MainPage(BasePage):
         search_field = self.click_element(locator)
         search_field.send_keys(word)
         return search_field
+
+    def open_tab_add_customer(self):
+        self.click_element(Locators.LCTR_TAB_ADD_CUSTOMER)
+
+    def open_tab_customers(self):
+        self.click_element(Locators.LCTR_TAB_CUSTOMERS)
+
+    def filling_fields(self):
+        post_code = get_post_code()
+        first_name = get_first_name(post_code)
+        last_name = first_name[::-1]
+
+        self.enter_word(Locators.LCTR_FIELD_FIRST_NAME, first_name)
+        self.enter_word(Locators.LCTR_FIELD_LAST_NAME, last_name)
+        self.enter_word(Locators.LCTR_FIELD_POST_CODE, post_code)
+
+    def click_add_customer(self):
+        self.click_element(Locators.LCTR_BTN_ADD_CUSTOMER)
+
+    def read_alert(self):
+        alert_obj = self.driver.switch_to.alert
+        msg = alert_obj.text
+        alert_obj.accept()
+        return msg
+
+    def sort_by_first_name(self):
+        self.click_element(Locators.LCTR_SORT_FIRST_NAME)
+
+    def get_customers_names(self) -> list[str]:
+        locator = Locators.LCTR_CUSTOMERS_TABLE
+        customers_names = []
+
+        rows = len(self.find_elements((locator[0], f'{locator[1]}')))
+
+        for r in range(1, rows + 1):
+            first_name = self.find_element((locator[0], f'{locator[1]}[{r}]/td[{1}]')).text
+            customers_names.append(first_name)
+
+        return customers_names
+
+    def delete_customer(self, name_to_delete):
+        locator = Locators.LCTR_CUSTOMERS_TABLE
+
+        rows = len(self.find_elements((locator[0], f'{locator[1]}')))
+        columns = len(self.find_elements((locator[0], f'{locator[1]}[1]/td')))
+
+        for r in range(1, rows + 1):
+            first_name = self.find_element((locator[0], f'{locator[1]}[{r}]/td[{1}]')).text
+            if first_name == name_to_delete:
+                delete_btn = self.find_element((locator[0], f'{locator[1]}[{r}]/td[{columns}]/button'))
+                delete_btn.click()
+
 
     def get_customers_del_btns(self):
         '''Метод, который считывает таблицу "Customer" и возвращает словарь,
--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -1,76 +1,51 @@
+from re import match
+
 import allure
 
-from helpers.help_functions import get_first_name, get_name_to_delete, get_post_code
-from pages.main_page import Locators, MainPage
-from re import  match
+from helpers.help_functions import get_name_to_delete
+from pages.main_page import MainPage
 
 
 @allure.epic("TestsMainPage")
 @allure.feature("Test Cases")
 class TestsMainPage:
-    '''Класс, описывающий автотест страницы MainPage'''
-    @allure.epic("TestsMainPage")
-    @allure.feature("Test Cases")
+    """Класс, описывающий автотест страницы MainPage"""
     @allure.story("Создание клиента Add Customer")
     def test_case_1(self, browser):
-        '''Тест-кейс 1. Создание клиента (Add Customer)'''
-        self.main_page = MainPage(browser)
-        self.main_page.go_to_site()
+        """Тест-кейс 1. Создание клиента (Add Customer)"""
+        main_page = MainPage(browser)
+        main_page.go_to_site()
 
-        with allure.step('Клик по вкладке Add "Customer"'):
-            self.main_page.click_element(Locators.LCTR_TAB_ADD_CUSTOMER)
+        main_page.open_tab_add_customer()
+        main_page.filling_fields()
+        main_page.click_add_customer()
+        alert_text = main_page.read_alert()
 
-        post_code = get_post_code()
-        first_name = get_first_name(post_code)
-        last_name = first_name[::-1]
+        assert bool(match(r'Customer added successfully', alert_text)), "Клиент не добавлен"
 
-        with allure.step('Ввод данных в поля "First Name", "Last Name", "Post Code"'):
-            self.main_page.enter_word(Locators.LCTR_FIELD_FIRST_NAME, first_name)
-            self.main_page.enter_word(Locators.LCTR_FIELD_LAST_NAME, last_name)
-            self.main_page.enter_word(Locators.LCTR_FIELD_POST_CODE, post_code)
-
-        with allure.step('Клик по кнопке "Add Customer"'):
-            self.main_page.click_element(Locators.LCTR_BTN_ADD_CUSTOMER)
-
-        alert_obj = browser.switch_to.alert
-        msg = alert_obj.text
-        alert_obj.accept()
-        assert bool(match(r'Customer added successfully', msg))
-
-    @allure.epic("TestsMainPage")
-    @allure.feature("Test Cases")
     @allure.story("Сортировка клиентов по имени First Name")
     def test_case_2(self, browser):
-        '''Тест-кейс 2. Сортировка клиентов по имени (First Name)'''
+        """Тест-кейс 2. Сортировка клиентов по имени (First Name)"""
         main_page = MainPage(browser)
-        with allure.step('Клик по вкладке "Customer"'):
-            main_page.click_element(Locators.LCTR_TAB_CUSTOMERS)
 
-        with allure.step('Первый клик по заголовку столбца "First Name"'):
-            main_page.click_element(Locators.LCTR_SORT_FIRST_NAME)
+        main_page.open_tab_customers()
+        main_page.sort_by_first_name()
 
-        with allure.step('Второй клик по заголовку столбца "First Name"'):
-            main_page.click_element(Locators.LCTR_SORT_FIRST_NAME)
+        customers_names = main_page.get_customers_names()
+        sorted_names = sorted(customers_names, key=lambda name: name.lower, reverse=True)
+        assert sorted_names == customers_names, "Клиенты не отсортированы"
 
-        customers_del_btns = main_page.get_customers_del_btns()
-        sorted_names = sorted(customers_del_btns.keys(), key=str.lower)
-        assert sorted_names == list(customers_del_btns.keys())
-
-    @allure.epic("TestsMainPage")
-    @allure.feature("Test Cases")
     @allure.story("Удаление клиента")
     def test_case_3(self, browser):
-        '''Тест-кейс 3. Удаление клиента'''
+        """Тест-кейс 3. Удаление клиента"""
         main_page = MainPage(browser)
 
-        with allure.step('Поиск клиента для удаления'):
-            customers_del_btns = main_page.get_customers_del_btns()
-            name_to_delete = get_name_to_delete(customers_del_btns.keys())
+        customers_names = main_page.get_customers_names()
+        name_to_delete = get_name_to_delete(customers_names)
 
-        with allure.step('Удаление выбранного клиента'):
-            customers_del_btns[name_to_delete].click()
+        main_page.delete_customer(name_to_delete)
 
-        assert name_to_delete not in main_page.get_customers_del_btns()
+        assert name_to_delete not in main_page.get_customers_names(), f"Клиент {name_to_delete} не удален"
 
 
 
