908d5bfe2dc0e4c7e2077d36557c02f6a0b6e40c
vadim_makarov <makarov.vadim.evph@gmail.com>
2025-03-31 22:37:46 +0300

    Тест коммит

--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -23,30 +23,30 @@ class TestsMainPage:
 
         assert bool(match(r'Customer added successfully', alert_text)), "Клиент не добавлен"
 
-    @allure.story("Сортировка клиентов по имени First Name")
-    def test_case_2(self, browser):
-        """Тест-кейс 2. Сортировка клиентов по имени (First Name)"""
-        main_page = MainPage(browser)
-
-        main_page.open_tab_customers()
-        main_page.sort_by_first_name()
-
-        customers_names = main_page.get_customers_names()
-        sorted_names = sorted(customers_names, key=lambda name: name.lower(), reverse=True)
-        assert sorted_names == customers_names, "Клиенты не отсортированы"
-
-    @allure.story("Удаление клиента")
-    def test_case_3(self, browser):
-        """Тест-кейс 3. Удаление клиента"""
-        main_page = MainPage(browser)
-
-        customers_names = main_page.get_customers_names()
-        name_to_delete = get_name_to_delete(customers_names)
-
-        main_page.delete_customer(name_to_delete)
-
-        assert name_to_delete not in main_page.get_customers_names(), f"Клиент {name_to_delete} не удален"
-
+    # @allure.story("Сортировка клиентов по имени First Name")
+    # def test_case_2(self, browser):
+    #     """Тест-кейс 2. Сортировка клиентов по имени (First Name)"""
+    #     main_page = MainPage(browser)
+    #
+    #     main_page.open_tab_customers()
+    #     main_page.sort_by_first_name()
+    #
+    #     customers_names = main_page.get_customers_names()
+    #     sorted_names = sorted(customers_names, key=lambda name: name.lower(), reverse=True)
+    #     assert sorted_names == customers_names, "Клиенты не отсортированы"
+    #
+    # @allure.story("Удаление клиента")
+    # def test_case_3(self, browser):
+    #     """Тест-кейс 3. Удаление клиента"""
+    #     main_page = MainPage(browser)
+    #
+    #     customers_names = main_page.get_customers_names()
+    #     name_to_delete = get_name_to_delete(customers_names)
+    #
+    #     main_page.delete_customer(name_to_delete)
+    #
+    #     assert name_to_delete not in main_page.get_customers_names(), f"Клиент {name_to_delete} не удален"
+    #
 
 
 
