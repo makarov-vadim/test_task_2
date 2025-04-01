760aa5a6c2ce47cbd64dc62f47e2f5ec00bc80e5
vadim_makarov <makarov.vadim.evph@gmail.com>
2025-04-01 21:17:01 +0300

    Тест коммит

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,28 +7,28 @@ from selenium.webdriver.chrome.options import Options
 from pages.main_page import MainPage
 
 
-# @pytest.fixture(scope="session")
-# def browser():
-#    print("\nstart browser for test..")
-#    options = Options()
-#    options.add_argument('--headless')
-#    browser = webdriver.Chrome(options=options)
-#    yield browser
-#    print("\nquit browser..")
-#    browser.quit()
-
 @pytest.fixture(scope="session")
 def browser():
    print("\nstart browser for test..")
-   options = webdriver.ChromeOptions()
+   options = Options()
    options.add_argument('--headless')
-   browser = webdriver.Remote(
-      command_executor="http://localhost:4444",
-      options=options
-   )
+   browser = webdriver.Chrome(options=options)
    yield browser
    print("\nquit browser..")
    browser.quit()
+
+# @pytest.fixture(scope="session")
+# def browser():
+#    print("\nstart browser for test..")
+#    options = webdriver.ChromeOptions()
+#    options.add_argument('--headless')
+#    browser = webdriver.Remote(
+#       command_executor="http://localhost:4444",
+#       options=options
+#    )
+#    yield browser
+#    print("\nquit browser..")
+#    browser.quit()
 
 @pytest.fixture(scope="session")
 def main_page(browser):
